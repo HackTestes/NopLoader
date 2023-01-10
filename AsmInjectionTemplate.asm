5208d89f3935290c688623fdbf4cabe05aa6ad14
HackTestes <caio.hacktestes@gmail.com>
2023-01-09 23:45:54 -0300

    Asm code redirection module (incomplete) correction

--- AsmInjectionTemplate.asm
+++ AsmInjectionTemplate.asm
@@ -1,22 +1,23 @@
 .code
 main PROC
 
-	;INJECTION POINT
-	push rax					;SAVING REGISTER VALUE
-	mov rax, 0FF000000000000FFh ;ADDRESS - fixed size
-	jmp rax
-	pop rax 					;RESTORE RAX AFTER JMP BACK
+    ;INJECTION POINT
+    push rax                    ;SAVING REGISTER VALUE
+    mov rax, 0FF000000000000FFh ;ADDRESS - fixed size
+    jmp rax
+    pop rax                     ;RESTORE RAX AFTER JMP BACK
 
-	;REDIRECTED CODE
-	pop rax                      ;RESTORE REGISTER AFTER JMP
-	nop							 ;CODE
-	nop							 ;RESTORE PARTS OF ORIGINAL IF NEEDED
-	push rax					 ;SAVING REGISTER VALUE
-	mov rax, 0FF000000000000FFh  ;ADDRESS = INJECTION ADDRESS + 13 BYTES
-	jmp rax
+    ;REDIRECTED CODE
+    pop rax                      ;RESTORE REGISTER AFTER JMP
+    nop                          ;CODE
+    nop                          ;RESTORE PARTS OF ORIGINAL IF NEEDED
+    push rax                     ;SAVING REGISTER VALUE
+    mov rax, 0FF000000000000FFh  ;ADDRESS = INJECTION ADDRESS + 13 BYTES
+    jmp rax
 
-	;"NEW CODE"
-	mov rax, 01h
-	inc rax
+
+    ;"NEW CODE"
+    mov rax, 01h
+    inc rax
 main ENDP
 END
